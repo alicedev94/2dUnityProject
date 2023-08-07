1e1296fd5b18450a185d6c4fe9300b651ecb6ad3
developmenttiendasdaka <development.tiendas.daka@gmail.com>
2023-08-06 20:50:45 -0400

    2dUnityProject 06-08-2023 20:50

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool canJump;
+
     // Start is called before the first frame update
     void Start()
     {
-		
-	}
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-		gameObject.transform.position = new Vector3(gameObject.transform.position.x + 50f * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
-	}
+        if (Input.GetKey("left"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-10000f * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey("right"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(10000f * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKeyDown("up") && canJump)
+        {
+            canJump = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5000f));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "ground")
+        {
+            canJump = true;
+        }
+    }
 }
